a5dd61f642d2511a0f780745a1139ec112f087e0
yudiwbs <yudi1975@gmail.com>
2014-12-08 20:54:26 +0700

    lsa2

--- src/edu/upi/cs/yudiwbs/rte/ToolsTableToFile.java
+++ src/edu/upi/cs/yudiwbs/rte/ToolsTableToFile.java
@@ -11,65 +11,11 @@ public class ToolsTableToFile {
   //memindahkan dari table ke 
 
 
-  //file teks
-  // nama filenya 1-h.txt dan 1-t.txt  untuk id 1 teks dan id 1 hypo
-   public void tableToFile(String namaTabelUtama, String path) {
-		Connection conn=null;
-		PreparedStatement pStat=null;
-		PreparedStatement pUpdate=null;
-		
-		ResultSet rs = null;
-		
-		//ambil data 
-		//PreparedStatement pUpdate=null;
-		try {
-		   		Class.forName("com.mysql.jdbc.Driver");
-		   		// Setup the connection with the DB
-		   		conn = DriverManager.getConnection("jdbc:mysql://localhost/textualentailment?"
-		   			   					+ "user=textentailment&password=textentailment");
-			    
-		   		String sql = "select id,t,h "
-		   				+ " from "+ namaTabelUtama;   				
-	
-		   		
-		   		pStat = conn.prepareStatement(sql);
-				rs = pStat.executeQuery();
-				
-				
-				
-				//loop semua rec 
-				while (rs.next()) {
-						int id = rs.getInt(1);
-					    String t       = rs.getString(2);  //parsetree text
-				        String h       = rs.getString(3);  //parsetree hypo
-					    
-					    System.out.println("----");
-					    System.out.println("Proses id:"+id);
-					    System.out.println(t);
-					    System.out.println(h);
-					    
-					    String namaFileOutT = path+id + "-" + "t.txt";
-					    String namaFileOutH = path+id + "-" + "h.txt";
-					    
-					    PrintWriter pwT = new PrintWriter(namaFileOutT);
-					    pwT.println(t);
-					    pwT.close();
-					    
-					    PrintWriter pwH = new PrintWriter(namaFileOutH);
-					    pwH.println(h);
-					    pwH.close();
-				}
-		   		rs.close();
-		   		pStat.close();
-		   		conn.close();
-		   	   } catch (Exception ex) {
-				   ex.printStackTrace();
-			   }
-	}
+
 	
 	public static void main(String[] args ) {
 		ToolsTableToFile tt = new ToolsTableToFile();
-		tt.tableToFile("testset_rte3_ver1_coba4", "G:\\eksperimen\\textualentailment\\rtedocs_testset\\");
+		//tt.tableToFile("testset_rte3_ver1_coba4", "G:\\eksperimen\\textualentailment\\rtedocs_testset\\");
 	}
 	
 }
